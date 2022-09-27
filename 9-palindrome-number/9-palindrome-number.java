e9e3d8bb7724199941c824db43833825f086e743
RAVI TEJA TADIBOINA <90016458+ravi347@users.noreply.github.com>
2022-09-27 17:19:06 -0400

    Time: 14 ms (68.78%), Space: 44.5 MB (57.46%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int num = x,rev = 0;
+        while(num != 0){
+            rev = (rev*10) + num%10;
+            num /= 10;
+        }
+        if(x == rev)
+            return true;
+        
+    return false;
+    }
+}
