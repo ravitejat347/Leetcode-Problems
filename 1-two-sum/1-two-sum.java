d22ac23a422380a72a09f1576b89da517f72dfaf
RAVI TEJA TADIBOINA <90016458+ravi347@users.noreply.github.com>
2022-09-27 17:09:52 -0400

    Time: 5 ms (71.02%), Space: 46.1 MB (17.80%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<nums.length; i++){
+            int x = target - nums[i];
+            if(map.containsKey(x)){
+                return new int[]{map.get(x), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
