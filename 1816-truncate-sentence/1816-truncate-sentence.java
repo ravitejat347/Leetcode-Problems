99db4134af9dea073a6789a42dc9ab9bf189555c
RAVI TEJA TADIBOINA <90016458+ravitejat347@users.noreply.github.com>
2022-10-02 14:54:32 -0400

    Time: 16 ms (6.39%), Space: 43.8 MB (9.57%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int n = s.length(),count = 0, i;
+        String res = "";
+        char[] str = s.toCharArray();
+        for(i=0; i<n; i++){
+            if(str[i] == ' '){
+                count++;
+            }
+            if(count == k){
+                break;
+            }
+        }
+        for(int j=0; j<i; j++){
+            res += s.charAt(j);
+        }
+        return res;
+    }
+}
