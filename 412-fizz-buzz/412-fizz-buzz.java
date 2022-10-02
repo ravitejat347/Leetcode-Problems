bd2c8ede3b423fa1984f33a4932b8990daf3d9b2
RAVI TEJA TADIBOINA <90016458+ravitejat347@users.noreply.github.com>
2022-10-01 21:24:46 -0400

    Time: 3 ms (43.99%), Space: 48.3 MB (48.33%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ls = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(i%3 == 0 && i%5 == 0){
+                ls.add("FizzBuzz");
+            } else if(i%3 == 0){
+                ls.add("Fizz");
+            } else if(i%5 == 0){
+                ls.add("Buzz");
+            } else {
+                ls.add(Integer.toString(i));
+            }
+        }
+        return ls;
+    }
+}
