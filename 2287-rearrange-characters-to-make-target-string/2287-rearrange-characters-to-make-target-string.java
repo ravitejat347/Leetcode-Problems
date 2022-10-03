ce9bb26739e2e0263ae8c31230dd85a0ba9e169a
RAVI TEJA TADIBOINA <90016458+ravitejat347@users.noreply.github.com>
2022-10-03 17:44:24 -0400

    Time: 8 ms (5.02%), Space: 42.5 MB (18.67%) - LeetHub

---
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rearrangeCharacters(String s, String target) {
+        int m = s.length(), n = target.length(), res = Integer.MAX_VALUE;
+        int[] sfreq = new int[26],tfreq = new int[26];
+        for(int i=0; i<m; i++){
+            sfreq[s.charAt(i) - 'a']++;
+        }
+        for(int i=0; i<n; i++){
+            tfreq[target.charAt(i) - 'a']++;
+        }
+        for(int i=0; i<n; i++){
+            res = Math.min
+                (res, sfreq[target.charAt(i)-'a']/tfreq[target.charAt(i)-'a']);
+            System.out.println(res);
+        }
+        return res;
+    }
+}
