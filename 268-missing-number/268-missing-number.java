138225ff22297d8173195b1b675334eef622c361
RAVI TEJA TADIBOINA <90016458+ravitejat347@users.noreply.github.com>
2023-01-05 19:54:59 -0500

    Time: 0 ms (100.00%), Space: 43.2 MB (71.47%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,15 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length, res =0;
-        int[] arr = new int[n+1];
-        for(int i=0; i<n; i++){
-            arr[nums[i]] = -1;
+        int expsum = 0, actsum = 0;
+        for(int i=0; i <= nums.length; i++){
+            expsum += i;
         }
-        for(int i=0; i<n+1; i++){
-            if(arr[i] != -1){
-                res = i;
-            }
+        for(int i=0; i<nums.length; i++){
+            actsum += nums[i];
         }
-        return res;
+        return expsum-actsum;
     }
 }
