c0db129f0e190abcd82e1fa4d76f2f71785e1e3e
RAVI TEJA TADIBOINA <90016458+ravitejat347@users.noreply.github.com>
2023-01-06 17:55:40 -0500

    Time: 1 ms (100.00%), Space: 41.8 MB (99.67%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length, ans = 0;
+        for(int i=0; i<n; i++){
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
