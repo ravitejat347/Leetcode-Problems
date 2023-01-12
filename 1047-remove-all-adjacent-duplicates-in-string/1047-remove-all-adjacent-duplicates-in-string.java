27387acf1d4aaa156f0e9715742423d76c006e8d
RAVI TEJA TADIBOINA <90016458+ravitejat347@users.noreply.github.com>
2023-01-11 22:15:48 -0500

    Time: 10 ms (94.22%), Space: 42.9 MB (86.50%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,18 +1,15 @@
 class Solution {
-    public String removeDuplicates(String s) {
-        Stack<Character> st = new Stack<>();
-        int n = s.length();
-        for(int i=0; i<n; i++){
-            if(st.isEmpty() || (!st.isEmpty() && s.charAt(i) != st.peek())){
-                st.push(s.charAt(i));
-            } else if(!st.isEmpty() && (s.charAt(i) == st.peek())){
-                    st.pop();
-            }
-        }
-        String ans = "";
-        for(char c: st){
-            ans += c;
-        }
-        return ans;
+  public String removeDuplicates(String S) {
+    StringBuilder sb = new StringBuilder();
+    int sbLength = 0;
+    for(char character : S.toCharArray()) {
+      if (sbLength != 0 && character == sb.charAt(sbLength - 1))
+        sb.deleteCharAt(sbLength-- - 1);
+      else {
+        sb.append(character);
+        sbLength++;
+      }
     }
+    return sb.toString();
+  }
 }
