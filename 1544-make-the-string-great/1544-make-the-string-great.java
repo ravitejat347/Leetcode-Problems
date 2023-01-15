80406015aefa5ce86c6869eb4f95e2c967850f17
RAVI TEJA TADIBOINA <90016458+ravitejat347@users.noreply.github.com>
2023-01-15 18:20:12 -0500

    Time: 3 ms (87.42%), Space: 40.9 MB (91.88%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char currChar : s.toCharArray()) {
+            if (!stack.isEmpty() && Math.abs(stack.peek() - currChar) == 32) 
+                stack.pop();
+            else
+                stack.add(currChar);
+        }
+        
+        StringBuilder ans = new StringBuilder();
+        for (char currChar : stack) 
+            ans.append(currChar);
+        return ans.toString();
+    }
+}
