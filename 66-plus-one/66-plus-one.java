14f53f731894a073892090d09b2888912ee34f72
RAVI TEJA TADIBOINA <90016458+ravitejat347@users.noreply.github.com>
2023-01-16 23:56:41 -0500

    Time: 0 ms (100.00%), Space: 41 MB (51.90%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int[] plusOne(int[] digits) {
+    int n = digits.length;
+
+    for (int idx = n - 1; idx >= 0; idx--) {
+     if (digits[idx] == 9) {
+        digits[idx] = 0;
+      } else {
+        digits[idx]++;
+        return digits;
+      }
+    }
+    digits = new int[n + 1];
+    digits[0] = 1;
+    return digits;
+  }
+}
