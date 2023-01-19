eeead08054861c01fc0da65de36395801df2390b
RAVI TEJA TADIBOINA <90016458+ravitejat347@users.noreply.github.com>
2023-01-19 02:44:35 -0500

    Time: 1 ms (100.00%), Space: 49.8 MB (79.48%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int i = 0, j = 0, n = nums.length, sum = 0, ans = Integer.MAX_VALUE, flag=0;
+        while(j < n){
+            sum += nums[j];
+            if(sum < target){
+                j++;
+            } else if(sum == target){
+                flag = 1;
+                ans = Math.min(j-i+1, ans);
+                //System.out.println(ans);
+                j++;
+            } else if(sum > target){
+                while(sum > target){
+                    flag =1;
+                    ans = Math.min(j-i+1, ans);
+                    sum -= nums[i];
+                    i++;
+                    if(sum >= target){
+                        //System.out.println(sum);
+                        flag = 1;
+                        ans = Math.min(j-i+1, ans);
+                    }
+                }
+                j++;
+            }
+        }
+        if(flag == 1){
+            return ans;
+        }
+        return 0;
+    }
+}
